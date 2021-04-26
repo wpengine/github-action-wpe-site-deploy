3b728149f22f8b8bd8239240384c123635a7a399
Zuniga <alex@bowiecreative.co>
2021-04-26 15:23:27 -0500

    connect with ID ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,5 +46,5 @@ chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
 rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
 
 # Clear cache 
-ssh $WPE_SSH_HOST "cd sites/${WPE_ENV_NAME} && wp page-cache flush"
+ssh -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no $WPE_SSH_HOST "cd sites/${WPE_ENV_NAME} && wp page-cache flush"
 echo "SUCCESS: Site has been deployed and cache has been flushed."
