5ee9b2546b513a7915b249269d1b6109dc685ae0
Zuniga <alex@bowiecreative.co>
2021-04-28 17:28:07 -0500

    simplify destination

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ fi
 
 WPE_SSH_USER="$WPE_ENV_NAME"@"$WPE_SSH_HOST"
 
-WPE_DESTINATION="$WPE_ENV_NAME"@"$WPE_SSH_HOST":sites/"$WPE_ENV_NAME"/"$DIR_PATH"
+WPE_DESTINATION="$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/"$DIR_PATH"
 
 # Setup our SSH Connection & use keys
 mkdir "$SSH_PATH"
