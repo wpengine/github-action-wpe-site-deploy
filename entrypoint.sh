f5c9f16c9ba81ee6606ce7934db2445a4d0a886d
MustyMouse <admin@krux.us>
2021-07-14 21:52:02 -0500

    Entrypoint test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -l
 
-set -e
-
-: ${WPE_ENV_NAME?Required environment name variable not set.}
-: ${WPE_SSHG_KEY_PRIVATE?Required secret not set.}
-: ${WPE_SSHG_KEY_PUBLIC?Required secret not set.}
-
 #SSH Key Vars 
 SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
@@ -14,21 +8,21 @@ WPE_SSHG_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
 
 #Deploy Vars
 WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
-if [ -n "$TPO_PATH" ]; then 
-    DIR_PATH="$TPO_PATH"
-else 
-    DIR_PATH=""
-fi
-
 if [ -n "$TPO_SRC_PATH" ]; then
     SRC_PATH="$TPO_SRC_PATH"
+else 
+    SRC_PATH=""
+fi
+
+if [ -n "$TPO_PATH" ]; then
+    DEST_PATH="$TPO_PATH"
 else
-    SRC_PATH="."
+    DEST_PATH=""
 fi
 
 WPE_SSH_USER="$WPE_ENV_NAME"@"$WPE_SSH_HOST"
 
-WPE_DESTINATION="$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/"$DIR_PATH"
+WPE_DESTINATION="$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/"$DEST_PATH"
 
 # Setup our SSH Connection & use keys
 mkdir "$SSH_PATH"
@@ -44,6 +38,9 @@ chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPE_SSHG_KEY_PRIVATE_PATH"
 chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
 
+
+echo "$SRC_PATH"
+echo "$DEST_PATH"
 # Deploy via SSH
 rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
 
