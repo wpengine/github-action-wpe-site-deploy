642c7274659134e8536dfe7c584c2f64d3dbaaa8
MustyMouse <admin@krux.us>
2021-07-15 15:42:56 -0500

    lint updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,16 +9,16 @@ SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
 WPE_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
 
-"""
-If you'd like to expand the environments, 
-Just copy/paste an elif line and the following export
-Then adjust variables to match the new ones you added in main.yml
-
-Example:
-
+###
+# If you'd like to expand the environments, 
+# Just copy/paste an elif line and the following export
+# Then adjust variables to match the new ones you added in main.yml
+#
+# Example:
+#
 # elif [[ {GITHUB_REP} =~ {NEW_BRANCH_NAME}$ ]]; then
 #     export WPE_ENV_NAME={NEW_ENV_NAME};    
-"""
+###
 
 if [[ $GITHUB_REF =~ ${PRD_BRANCH}$ ]]; then
     export WPE_ENV_NAME=$PRD_ENV;
