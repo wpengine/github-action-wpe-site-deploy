96e8ebad5730bff26b008cfeaa217fe4cf2b05f2
Christian Romero <47224821+christian-wpengine@users.noreply.github.com>
2021-07-15 22:45:47 -0500

    Feature/hackathon (#5)
    * cleanup readme
    
    * remove public key requirement
    
    * remove public key refs
    
    
    Co-authored-by: Zuniga <alex@bowiecreative.co>
    Co-authored-by: Brett Krueger <admin@krux.us>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,23 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 set -e
 
-: ${WPE_ENV_NAME?Required environment name variable not set.}
 : ${WPE_SSHG_KEY_PRIVATE?Required secret not set.}
-: ${WPE_SSHG_KEY_PUBLIC?Required secret not set.}
 
 #SSH Key Vars 
 SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
 WPE_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
-WPE_SSHG_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
+
+if [[ $GITHUB_REF =~ ${PRD_BRANCH}$ ]]; then
+    export WPE_ENV_NAME=$PRD_ENV;
+elif [[ $GITHUB_REF =~ ${STG_BRANCH}$ ]]; then
+    export WPE_ENV_NAME=$STG_ENV;
+elif [[ $GITHUB_REF =~ ${DEV_BRANCH}$ ]]; then
+    export WPE_ENV_NAME=$DEV_ENV;    
+else 
+    echo "FAILURE: Branch name required." && exit 1;
+fi
 
 #Deploy Vars
 WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
@@ -26,8 +33,9 @@ else
     SRC_PATH="."
 fi
 
-WPE_SSH_USER="$WPE_ENV_NAME"@"$WPE_SSH_HOST"
+# Set up our user and path
 
+WPE_SSH_USER="$WPE_ENV_NAME"@"$WPE_SSH_HOST"
 WPE_DESTINATION="$WPE_SSH_USER":sites/"$WPE_ENV_NAME"/"$DIR_PATH"
 
 # Setup our SSH Connection & use keys
@@ -36,16 +44,21 @@ ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 
 #Copy Secret Keys to container
 echo "$WPE_SSHG_KEY_PRIVATE" > "$WPE_SSHG_KEY_PRIVATE_PATH"
-echo "$WPE_SSHG_KEY_PUBLIC" > "$WPE_SSHG_KEY_PUBLIC_PATH"
 
 #Set Key Perms 
 chmod 700 "$SSH_PATH"
 chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPE_SSHG_KEY_PRIVATE_PATH"
-chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
+
+# Lint before deploy
+if [ "$PHP_LINT" == true ]; then
+    echo "Begin PHP Linting."
+    php -l $SRC_PATH
+    echo "End PHP Linting."
+fi
 
 # Deploy via SSH
-rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
+rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --inplace --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
 
 # Clear cache 
 ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no $WPE_SSH_USER "cd sites/${WPE_ENV_NAME} && wp page-cache flush"
