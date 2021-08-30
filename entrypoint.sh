bf97f817a961b9099506ac4224c14f1be802c55b
Zuniga <alex@bowiecreative.co>
2021-08-30 17:11:27 -0500

    dump var for cache clear

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,8 +81,10 @@ else
 fi
 
 # Deploy via SSH
-rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" $INPUT_FLAGS $SRC_PATH "$WPE_DESTINATION"
+#rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" $INPUT_FLAGS $SRC_PATH "$WPE_DESTINATION"
 
+echo "Cache clear is set to:"
+echo $CACHE_CLEAR
 
 # Clear cache 
 if [ "${CACHE_CLEAR^^}" == "TRUE" ]; then
