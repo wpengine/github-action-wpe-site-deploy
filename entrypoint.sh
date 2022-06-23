ead4e849a55335910263eb4a9b051d192fcf7057
marksabbath <marksabbath@gmail.com>
2022-06-23 13:57:52 -0300

    Fix syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -100,7 +100,7 @@ if [[ -n ${SCRIPT} || -n ${CACHE_CLEAR} ]]; then
     if [[ -n ${SCRIPT} ]]; then
       ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o ControlPath="$SSH_PATH/ctl/%C" $WPE_FULL_HOST "stat sites/${WPE_ENV_NAME}/${INPUT_SCRIPT}"
 
-      if [[ $? -ne 0]] && [[ -f ${INPUT_SCRIPT} ]]; then
+      if [[ $? -ne 0 && -f ${INPUT_SCRIPT} ]]; then
         rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o 'ControlPath=$SSH_PATH/ctl/%C'" ${INPUT_SCRIPT} sites/${WPE_ENV_NAME}/${INPUT_SCRIPT}
       fi
     fi
