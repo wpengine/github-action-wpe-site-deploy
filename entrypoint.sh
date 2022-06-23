35d7ea324fb76c5a506aebc343a187c1f96e09b2
marksabbath <marksabbath@gmail.com>
2022-06-23 14:21:22 -0300

    echoing INPUT_SCRIPT

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,6 +102,7 @@ if [[ -n ${SCRIPT} || -n ${CACHE_CLEAR} ]]; then
 
       # if [[ $? -ne 0 && -f ${INPUT_SCRIPT} ]]; then
       if [[ -f ${INPUT_SCRIPT} ]]; then
+      echo ${INPUT_SCRIPT}
         rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no -o 'ControlPath=$SSH_PATH/ctl/%C'" ${INPUT_SCRIPT} sites/${WPE_ENV_NAME}/${INPUT_SCRIPT}
       fi
     fi
